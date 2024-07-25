752fb5f664501608a69cff6b1f3238f8d90931f0
Priyankamalviya16 <priyankamalviya2004@gmail.com>
2024-07-25 13:22:10 +0530

    commiting the changes to local repo

---
+++ Pihu2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to calculate its factorial: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " = " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        }
+        
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
